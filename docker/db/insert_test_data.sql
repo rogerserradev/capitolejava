b8416b1817aca80b5343e6fa00d6c22d2b958caa
Roger <rserra.games@gmail.com>
2026-04-19 17:56:07 +0200

    test data

---
+++ docker/db/insert_test_data.sql
@@ -0,0 +1,23 @@
+INSERT INTO product (name, description) VALUES
+('Coolway Track - Jacket', 'Loose fit track jacket. 330gsm double-sided fabric.'),
+('Vans Vivid Dreams - T-shirt', 'Vans Vivid Dreams Oversized T-shirt.'),
+('Denim tie-front top', 'Waistcoat-style denim top. Slightly dropped armholes. Crossover tie fastening in the same tone.'),
+('Wide-leg jeans', 'Mid-waist denim trousers. Elasticated waistband. Button and zip fastening. Pleat detail. Loose-fitting leg.'),
+('Toe heel sandals', 'Women''s heel sandals. Available in various colors. With stiletto heel.');
+
+INSERT INTO price (product_id, value, init_date, end_date) VALUES
+(1, 19.99, '2023-01-01', '2023-02-01'),
+(1, 14.99, '2023-04-01', '2023-05-01'),
+(2, 29.99, '2023-01-01', '2023-03-01'),
+(2, 39.99, '2023-03-01', '2023-04-01'),
+(2, 19.99, '2023-06-01', '2023-07-01'),
+(3, 9.99, '2023-02-01', '2023-03-01'),
+(3, 12.99, '2023-04-01', '2023-07-01'),
+(4, 49.99, '2024-01-01', '2024-12-01'),
+(4, 39.99, '2025-01-01', '2025-12-01'),
+(4, 34.99, '2026-01-01', '2026-02-01'),
+(5, 25.99, '2020-01-01', '2021-02-01'),
+(5, 20.99, '2022-01-01', '2022-03-01'),
+(5, 29.99, '2023-01-01', '2023-05-01'),
+(5, 19.99, '2024-01-01', '2024-08-01');
+
